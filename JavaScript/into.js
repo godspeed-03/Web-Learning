b347fada7716257a6101e5fef9ea7a0a6aea1e4c
godspeed-03 <satyam2001anand@gmail.com>
2024-06-22 22:35:35 +0530

    Update : Codebase

--- JavaScript/into.js
+++ JavaScript/into.js
@@ -14,7 +14,28 @@ func()
 console.log( typeof Symbol())
 
 console.log( [1, 2, 3].map((num, index) => (num)))
+console.log("3" + 1 +0)
+
+let a = 10;
+function f() {
+if(true){
+    var b = 9
+    console.log(b);
+}
+console.log(b);
+    console.log(a);
+}
+f();
+// console.log(b);
+console.log(a);
 
 /// datatypes 
 
 // let var const 
+
+
+const valuestring = 'stringvalue'
+
+const objstring = new String(valuestring)
+
+console.log(valuestring.length)
