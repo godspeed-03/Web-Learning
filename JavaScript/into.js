95eaac15a9838660ec7c4af0e4c28118e08dfacc
godspeed-03 <satyam2001anand@gmail.com>
2024-06-22 17:55:49 +0530

    Updated codebase

---
+++ JavaScript/into.js
@@ -0,0 +1,20 @@
+//  lets see how to use let, var and cost and their scope
+
+console.log('21 june 2024')
+
+
+function func(){
+    var account = 'local state';
+    let scope = 'scope let'
+    console.log(account, scope)
+}
+
+func()
+
+console.log( typeof Symbol())
+
+console.log( [1, 2, 3].map((num, index) => (num)))
+
+/// datatypes 
+
+// let var const 
