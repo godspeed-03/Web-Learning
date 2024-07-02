b7e9049b1d5c93da7b645fd021d6e52dddbc2804
godspeed-03 <satyam2001anand@gmail.com>
2024-07-02 13:44:19 +0530

    Updated folder and codebase

--- JavaScript/into.js
+++ JavaScript/into.js
@@ -1,37 +1,37 @@
-//  lets see how to use let, var and cost and their scope
+// //  lets see how to use let, var and cost and their scope
 
-console.log('21 june 2024')
+// console.log('21 june 2024')
 
 
-function func(){
-    var account = 'local state';
-    let scope = 'scope let'
-    console.log(account, scope)
-}
+// function func(){
+//     var account = 'local state';
+//     let scope = 'scope let'
+//     console.log(account, scope)
+// }
 
-func()
+// func()
 
-console.log( typeof Symbol())
+// console.log( typeof Symbol())
 
-console.log( [1, 2, 3].map((num, index) => (num)))
-console.log("3" + 1 +0)
+// console.log( [1, 2, 3].map((num, index) => (num)))
+// console.log("3" + 1 +0)
 
-let a = 10;
-function f() {
-if(true){
-    var b = 9
-    console.log(b);
-}
-console.log(b);
-    console.log(a);
-}
-f();
+// let a = 10;
+// function f() {
+// if(true){
+//     var b = 9
+//     console.log(b);
+// }
 // console.log(b);
-console.log(a);
+//     console.log(a);
+// }
+// f();
+// // console.log(b);
+// console.log(a);
 
-/// datatypes 
+// /// datatypes 
 
-// let var const 
+// // let var const 
 
 
 const valuestring = 'stringvalue'
@@ -39,3 +39,14 @@ const valuestring = 'stringvalue'
 const objstring = new String(valuestring)
 
 console.log(valuestring.length)
+
+let value = "Lets say i have no value";
+ let result = value.trim().split("").forEach((ch) => {
+    console.log(ch)
+ })
+
+ let res =value.map((ch) => ch);
+ console.log(res)
+
+
+ 
