481a720dcf5ab031eb3afd859ebf1b0f9a0b007b
godspeed-03 <satyam2001anand@gmail.com>
2024-12-19 17:58:50 +0530

    updt: Document updated

---
+++ Typescript/first.ts
@@ -0,0 +1,16 @@
+let greetings : string = "hello string"
+
+// greetings =7
+
+class user {
+    email : string
+    age : number
+
+    constructor(email: string, age: number){
+        this.email = email
+        this.age = age
+    }
+    
+}
+
+// Genrics inTS
